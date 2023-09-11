89b0c5dd5cb16e00c4abd521501ac789f024d85f
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-09-11 21:37:26 +0000

    🎯 S01E02 Express Routes: Se agregaron estilos a aboutTec

--- views/TecScript.js
+++
@@ -1,9 +0,0 @@
-var objetos = new Array();
-    objetos[0] = "https://i.ibb.co/fHkdP7f/tec1.jpg";
-    objetos[1] = "https://i.ibb.co/CP0bS42/tec2.jpg";
-
-    function aleatorio() 
-    {
-        var azar = Math.floor(Math.random() * objetos.length);
-        document.images["imagen"].src = objetos[azar];
-    }
