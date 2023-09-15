59768157c8a14dec76ee05f44e9d06855f791182
lower <lower@LAPTOP-03B2J4QK>
2023-09-15 00:10:04 +0000

    🎯Avance: Webpack El primer checkpoint de la asignación Webpack front-end

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 //Exportamos un objeto de configuración Configuration Options Object
 module.exports = {
+    //-> Estableciendo el modo de produccion
+    mode: 'production',
     //Estableciendo el archivo indexador del front-end
     entry: "./client/index.js",
     //Estableciendo el archivo de salida
@@ -11,16 +13,5 @@ module.exports = {
         path: path.resolve(__dirname, "public"),
         //Nombre del archivo de salida
         filename: "bundle.js",
-        //Ruta base de archivos estaticos
-        publicPath: "/"
-    },
-    //Servidor de desarrollo
-    devServer: {
-        //Configurando el folder de estaticos
-        static: path.join(__dirname,"public"),
-        //Configurando el puerto del servidor de desarrollo
-        port: 8080,
-        //Host
-        host: '0.0.0.0'
     }
 }
