763065f547e6255f47ab5db9ba40647a853c236e
david <davidoisrael26@gmail.com>
2023-11-21 00:09:38 +0000

    🌠Enhance: techNotes Image

--- server/domains/home/home.controller.js
+++ server/domains/home/home.controller.js
@@ -2,14 +2,16 @@
 // GET "/"
 // GET "index"
 const home = (request, response) => {
-  const iconSet = ['🐤', '🤓', '🍇','🐈','🐶'];
+  const iconSet = ['🐤', '🤓', '🍇', '🐈', '🐶'];
   const icon = iconSet[Math.floor(Math.random() * 3)];
-  response.render('home/homeView', { icon });
+  response.render('home/homeView', {
+    icon,
+  });
 };
 
 // GET /about
 const about = (request, response) => {
-  response.render('home/aboutView', { appVersion: '1.0.0'});
+  response.render('home/aboutView', { appVersion: '1.0.0' });
 };
 
 // Controlador Home
